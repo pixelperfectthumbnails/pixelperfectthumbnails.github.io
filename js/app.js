b3cc23146341bc8d7771ade415c9db16125dda3e
Ramirezjosh <143044120+Ramirezjosh@users.noreply.github.com>
2024-02-19 15:45:16 -0600

    cooolcode

--- js/app.js
+++ js/app.js
@@ -8,17 +8,19 @@ let xValue = 0,
 window.addEventListener('mousemove', (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
-
+    if (yValue < -250) yValue = -250;
+    if (yValue > 110) yValue = 110;
     console.log(xValue, yValue);
 
     parralax_el.forEach((el) => {
-        let speedx = el.dataset.speedx;
+        let speedx = el.dataset.speedx*0;
         let speedy = el.dataset.speedy;
-       
+
+
         let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
 
         let zValue = e.clientX - parseFloat(getComputedStyle(el).left) * isInLeft * 0.1;
     
-        el.style.transform = `translateX(calc(-50% + ${xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+        el.style.transform = `translateX(calc(-50% + ${xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue*0}px)`;
 });
 });
