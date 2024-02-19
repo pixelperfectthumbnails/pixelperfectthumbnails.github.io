dd7aa0d5cd36e0fa37b7d559d04210693a15d9ff
Ramirezjosh <143044120+Ramirezjosh@users.noreply.github.com>
2024-02-19 15:23:53 -0600

    v2

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const parralax_el = document.querySelectorAll('.parralax');
+
+
+let xValue = 0,
+    yValue = 0;
+
+
+window.addEventListener('mousemove', (e) => {
+    xValue = e.clientX - window.innerWidth / 2;
+    yValue = e.clientY - window.innerHeight / 2;
+
+    console.log(xValue, yValue);
+
+    parralax_el.forEach((el) => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+       
+        let isInLeft = parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+
+        let zValue = e.clientX - parseFloat(getComputedStyle(el).left) * isInLeft * 0.1;
+    
+        el.style.transform = `translateX(calc(-50% + ${xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+});
+});
